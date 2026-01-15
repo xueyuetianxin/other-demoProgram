065ac7d38c4f80df0191f98d67085f56bf88069c
刘瑜梧 <liuyuwu53@gmail.com>
2026-01-15 21:32:27 +0800

    Update app

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,9 @@
-import { defineConfig } from 'vite'
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5176, // 修改为其他端口
+  },
+})import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
